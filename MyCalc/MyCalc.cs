0e05d46dd8b56c33a6295bc71476323b2d4bdbd1
Abhinav Mishra <E008599@cigniti.com>
2022-08-17 17:32:10 +0530

    Update

---
+++ MyCalc/MyCalc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LabExercise
+{
+    public class MyCalc
+    {
+        public static  double Add (double op1, double op2)
+        {
+            return op1 + op2;
+           
+        }
+        public static  double Sub (double op1, double op2)
+        {
+            return op1 - op2;
+           
+        }
+        public static  double Mul (double op1, double op2)
+        {
+            return op1 * op2 ;
+        }
+        public static  double  Div (double op1, double op2)
+        {
+            return op1 / op2 ;
+        }
+        public static  int  Pow (int n1, int n2)
+        {
+            int n3 = n1;
+            for (int i=1; i<n2; i++)
+            {
+                n3 = n3 * n1;
+            }
+            return n3;
+        }
+    }
+}
---
+++ MyCalcTest/MyCalcTest.cs
@@ -0,0 +1,89 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LabExercise;
+
+namespace LabExercise
+{
+    [TestClass]
+    public class MyCalcTest
+    {
+        
+        [DataTestMethod]
+        [DataRow(10,20,30)]
+        public void TestDataAddmethod(double a1,double a2,double expectedresult)
+        {
+            double actualresult = MyCalc.Add(a1,a2);
+        Assert.AreEqual(expectedresult,actualresult, "add Test Case not passes");
+        }
+        
+        [DataTestMethod]
+        [DataRow(30,20,10)]
+        public void TestDataSubmethod(double a1,double a2,double expectedresult)
+        {
+            double actualresult = MyCalc.Sub(a1,a2);
+        Assert.AreEqual(expectedresult,actualresult, "sub Test Case not passes");
+        }
+
+        [DataTestMethod]
+        [DataRow(10,20,200)]
+        public void TestDataMulmethod(double a1,double a2,double expectedresult)
+        {
+            double actualresult = MyCalc.Mul(a1,a2);
+            Assert.AreEqual(expectedresult,actualresult, "add Mul Case not passes");
+        }
+        
+        [DataTestMethod]
+        [DataRow(20,10,2)]
+        public void TestDataDivmethod(double a1,double a2,double expectedresult)
+        {
+            double actualresult = MyCalc.Div(a1,a2);
+            Assert.AreEqual(expectedresult,actualresult, "add Div Case not passes");
+        }
+        
+        [DataTestMethod]
+        [DataRow(5,3,125)]
+        public void TestDataPowmethod(int a1,int a2,int expectedresult)
+        {
+            int actualresult = MyCalc.Pow(a1,a2);
+            Assert.AreEqual(expectedresult,actualresult, "Pow Test Case not passes");
+        }
+        [DataTestMethod]
+        [DataRow(10,20,40)]
+        public void TestData1Addmethod(double a1,double a2,double expectedresult)
+        {
+            double actualresult = MyCalc.Add(a1,a2);
+        Assert.AreNotEqual(expectedresult,actualresult, "add Test Case not passes");
+        }
+        [DataTestMethod]
+        [DataRow(30,20,20)]
+        public void TestData1Submethod(double a1,double a2,double expectedresult)
+        {
+            double actualresult = MyCalc.Sub(a1,a2);
+        Assert.AreNotEqual(expectedresult,actualresult, "sub Test Case not passes");
+        }
+
+        [DataTestMethod]
+        [DataRow(10,20,300)]
+        public void TestData1Mulmethod(double a1,double a2,double expectedresult)
+        {
+            double actualresult = MyCalc.Mul(a1,a2);
+            Assert.AreNotEqual(expectedresult,actualresult, "add Mul Case not passes");
+        }
+        
+        [DataTestMethod]
+        [DataRow(20,10,4)]
+        public void TestData1Divmethod(double a1,double a2,double expectedresult)
+        {
+            double actualresult = MyCalc.Div(a1,a2);
+            Assert.AreNotEqual(expectedresult,actualresult, "add Div Case not passes");
+        }
+        
+        [DataTestMethod]
+        [DataRow(5,3,325)]
+        public void TestData1Powmethod(int a1,int a2,int expectedresult)
+        {
+            int actualresult = MyCalc.Pow(a1,a2);
+            Assert.AreNotEqual(expectedresult,actualresult, "Pow Test Case not passes");
+        }
+    
+    }
+}
